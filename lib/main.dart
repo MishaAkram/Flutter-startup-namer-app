4836900aca8456ae0d94cdc149dc973b03674d82
MishaAkram <mishaakrambaig@gmail.com>
2021-05-22 14:20:36 +0500

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
     // final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(          // Add the 3 lines from here... 
+        primaryColor: Colors.white,
+      ),                         // ... to here.
       home: RandomWords(),
     );
   }
